00ce513bb07ba01c3cef7b58d9dce0bbb7e7d2cd
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2022-07-04 15:01:08 +0800

    remove all file

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const server = require("http").Server(app)
-
-const PORT = process.env.PORT || 8000
-
-server.listen(PORT, () => {
-    console.log("Server running on port: " + PORT);
-})
